ad6432942a8ef3f9bf01a5853b0767dd86163c63
IKE1213 <ikedakoukun1213@gmail.com>
2025-11-13 10:26:21 +0900

    トークン解析処理完了

--- middleware.js
+++ middleware.js
@@ -1,8 +1,24 @@
+import { jwtDecrypt, jwtVerify } from "jose"
 import { NextResponse } from "next/server"
 
-export async function middleware(){
-    console.log("ミドルウェア")
-    return NextResponse.next()
+export async function middleware(request){
+    const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImV4cCI6MTc2MzA4MzIzOX0.H3KEuXYm90g_C-nj91wcFWox9G7dH9jWLIHE077LBVk"
+    
+    //request.headers.get("Autorization")?.split(" ")[1] //
+
+    if(!token){
+        return NextResponse.json({message: "トークンがありません"})
+    }
+
+    try{
+        const secretKey = new TextEncoder().encode("next-market-app-book")
+        const decodeJwt = await jwtVerify(token, secretKey)
+        // console.log("decodeJwt: ", decodeJwt)
+        return NextResponse.next()
+    }catch{
+        return NextResponse.json({message: "トークンが正しくないので、ログインしてください"})
+    }
+    
 }
 
 export const config = {
