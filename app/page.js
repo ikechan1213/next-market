19706ed4ba1af727a7d5f791fa4069e6c8f9d284
IKE1213 <ikedakoukun1213@gmail.com>
2025-12-18 09:51:11 +0900

    jsonData表示

--- app/page.js
+++ app/page.js
@@ -1,4 +1,13 @@
+const getAllItems = async () => {
+  const response = await fetch("http://localhost:3000/api/item/readall")
+  const jsonData = await response.json();
+  const allItems = jsonData.allItems;
+  return allItems;
+}
+
+
 const ReadAllItems = () => {
+  getAllItems()
   return( 
   <div>
     <h1 className="h1-style">こんにちは</h1>
