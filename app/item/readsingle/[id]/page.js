ef6b55dba054792ab4f38c05053044a87e12d78e
IKE1213 <ikedakoukun1213@gmail.com>
2025-12-18 12:05:07 +0900

    すべてのアイテムデータを読み込むページの表示

---
+++ app/item/readsingle/[id]/page.js
@@ -0,0 +1,7 @@
+const ReadAllItems = () => {
+    return(
+        <h1>個別アイテムページ</h1>
+    )
+}
+
+export default ReadSingleItem
--- app/page.js
+++ app/page.js
@@ -1,19 +1,30 @@
+import Link from "next/link"
+import Image from "next/image"
+
 const getAllItems = async () => {
   const response = await fetch("http://localhost:3000/api/item/readall",{
     cache: 'no-store'  })
   const jsonData = await response.json();
   const allItems = jsonData.allItems;
+  console.log(allItems);
   return allItems;
 }
 
 
 const ReadAllItems = async() => {
   const allItems = await getAllItems();
-  console.log(allItems);
   return( 
   <div>
-    <h1 className="h1-style">こんにちは</h1>
-    <h3>さようなら</h3>
+    {allItems.map(item => 
+      <Link href="" key={item._id}>
+        <Image src={item.image} width={750} height={500} alt="item-image" priority/>
+        <div>
+          <h2>{item.price}</h2>
+          <h3>{item.title}</h3>
+          <p>{item.description.substring(0, 80)}...</p>
+        </div>
+      </Link>
+  )}
   </div>
   )
 }
