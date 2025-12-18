b8e8d6f904868713a9b7a72c499e1c89b4ef5973
IKE1213 <ikedakoukun1213@gmail.com>
2025-12-18 10:12:20 +0900

    キャッシュを利用しないように設定

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 const getAllItems = async () => {
-  const response = await fetch("http://localhost:3000/api/item/readall")
+  const response = await fetch("http://localhost:3000/api/item/readall",{
+    cache: 'no-store'  })
   const jsonData = await response.json();
   const allItems = jsonData.allItems;
   return allItems;
 }
 
 
-const ReadAllItems = () => {
-  getAllItems()
+const ReadAllItems = async() => {
+  const allItems = await getAllItems();
+  console.log(allItems);
   return( 
   <div>
     <h1 className="h1-style">こんにちは</h1>
