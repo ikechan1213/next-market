da8f56f873dde64078b20d83cd632476b2ecde42
IKE1213 <ikedakoukun1213@gmail.com>
2025-12-18 12:32:21 +0900

    個別アイテムページcontext表示

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -1,4 +1,12 @@
-const ReadSingleItem = () => {
+// import { get } from "mongoose"
+
+const getSingleItem = (id) => {
+    const response = fetch("http://localhost:3000/api/item/readsingle/id")
+}
+
+const ReadSingleItem = (context) => {
+    const params = context.params
+    getSingleItem(params.id)
     return(
         <h1>個別アイテムページ</h1>
     )
